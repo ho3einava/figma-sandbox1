ed3dfbbdb2d6798ec2a876a2acdfac2b85b49c55
hoeinava <hossein.singer7@gmail.com>
2023-02-11 00:04:08 +0330

    commit 1 on branch master

---
+++ src/components/component/sideBar.vue
@@ -0,0 +1,21 @@
+<template>
+
+<img :src="url" class="img-1"><img :src="url" class="img-2">
+</template>
+
+<script>
+
+export default{
+data(){
+    return{
+        url : 'https://picsum.photos/200/300 ',
+    }
+}
+
+}    
+
+</script>
+
+<style src="../css/style.css">
+
+</style>
---
+++ src/components/page/homePage.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    <div class="sidebar">
+        <sidebar/>
+    </div>
+    <div class="content">content</div>
+    <div class="profile">profile</div>
+</div>
+
+</template>
+
+<script>
+import sidebar from '../component/sideBar.vue'
+
+export default {
+ components:{
+    sidebar
+ }
+}
+</script>
+
+<style src="../css/style.css">
+
+</style>
