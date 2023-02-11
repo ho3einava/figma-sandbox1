6bdb6f8b1d48b3de3ab4409d7357a9695e38b610
hoeinava <hossein.singer7@gmail.com>
2023-02-12 00:13:12 +0330

    commit2

--- src/components/component/sideBar.vue
+++ src/components/component/sideBar.vue
@@ -1,16 +1,26 @@
 <template>
-
-<img :src="url" class="img-1"><img :src="url" class="img-2">
+<div class="style-img">
+<img src="@/assets/img/Fill.svg" class="img-1"><img src="@/assets/img/SE.svg" class="img-2">
+<img src="@/assets/img/Ellipse1.svg" class="img-3"><img src="@/assets/img/T.svg" class="img-4">
+</div>
+<div >
+   <ul class="menu">
+    <li class="awesome"><a href="#"><i class="fa-solid fa-house"></i>Overview</a></li>
+   <li class="awesome"><a href="#"><i class="fa-solid fa-chart-simple"></i> Course</a></li> 
+    <li class="awesome"><a href="#"><i class="fa-regular fa-folder-closed">Resources</i></a></li>
+    <li class="awesome"><a href="#"><i class="fa-regular fa-message"></i>Message</a></li>
+    <li class="awesome"><a href="#"><i class="fa-solid fa-gear"></i>Setting</a></li>
+   </ul>
+</div>
+<div class="girl-img">
+    <img src="@/assets/img/Group.svg" alt="">
+</div>
 </template>
 
 <script>
 
+
 export default{
-data(){
-    return{
-        url : 'https://picsum.photos/200/300 ',
-    }
-}
 
 }    
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+
+/* Set up using Vue 3 */
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+
+
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app=createApp(App)
+app.mount('#app');
