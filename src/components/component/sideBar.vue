600b6d96a2b267716ef814662191b6a7dac9f672
hoeinava <hossein.singer7@gmail.com>
2023-02-15 00:21:20 +0330

    create sidebar

--- src/components/component/sideBar.vue
+++ src/components/component/sideBar.vue
@@ -3,17 +3,42 @@
 <img src="@/assets/img/Fill.svg" class="img-1"><img src="@/assets/img/SE.svg" class="img-2">
 <img src="@/assets/img/Ellipse1.svg" class="img-3"><img src="@/assets/img/T.svg" class="img-4">
 </div>
-<div >
-   <ul class="menu">
-    <li class="awesome"><a href="#"><i class="fa-solid fa-house"></i>Overview</a></li>
-   <li class="awesome"><a href="#"><i class="fa-solid fa-chart-simple"></i> Course</a></li> 
-    <li class="awesome"><a href="#"><i class="fa-regular fa-folder-closed">Resources</i></a></li>
-    <li class="awesome"><a href="#"><i class="fa-regular fa-message"></i>Message</a></li>
-    <li class="awesome"><a href="#"><i class="fa-solid fa-gear"></i>Setting</a></li>
-   </ul>
-</div>
+
+
+   <div class="main-menu">
+  <ul class="flex">
+   <div><li><i class="fa-solid fa-house"></i><span>Overview<i class="fa-solid fa-circle" id="circle" ></i></span></li></div> 
+   
+  </ul>
+  <ul class="flex">
+   <div><li><i class="fa-solid fa-layer-group"></i><span>Course<i class="fa-solid fa-circle" id="circle" ></i></span></li></div> 
+  </ul>
+  <ul class="flex">
+   <div><li><i class="fa-solid fa-folder"></i><span>Resources<i class="fa-solid fa-circle" id="circle" ></i></span></li></div> 
+  </ul>
+  <ul class="flex">
+   <div><li><i class="fa-solid fa-message"></i><span>Message<i class="fa-solid fa-circle" id="circle" ></i></span></li></div> 
+  </ul>
+  <ul class="flex">
+   <div><li><i class="fa-solid fa-gear"></i><span>Setting<i class="fa-solid fa-circle" id="circle" ></i></span></li></div> 
+  </ul>
+
+   </div>
+   
 <div class="girl-img">
     <img src="@/assets/img/Group.svg" alt="">
+</div>
+
+<div class="plan">
+    <span class="plan-text">Upgrade your plan</span>
+    <span class="plan-text2">Go to <b>PRO</b> </span>
+    <div class="chevrons-right">
+<i class="fa-solid fa-chevron-right"></i>
+<i class="fa-solid fa-chevron-right"></i>
+ 
+
+
+</div>
 </div>
 </template>
 
